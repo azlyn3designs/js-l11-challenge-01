e104af16629685f5ced6831d6ef4b3a06da4638d
thea_smith <smith.thea@outlook.com>
2021-09-02 11:09:16 -0600

    Initial commit - enable async function with Imgflip API

--- js/script.js
+++ js/script.js
@@ -2,3 +2,13 @@ const button = document.querySelector("button");
 const authorSpan = document.querySelector(".author");
 const imgDiv = document.querySelector(".image-container");
 const img = document.querySelector(".img");
+
+//declares getImage with Async Function
+const getImage = async function() {
+    const res = await fetch("https://picsum.photos/v2/list?limit=100"); //enables asynchronous communication w/Imgflip API
+    const images = await res.json(); //parse captured data 
+    console.log(images);
+};
+
+//calls getImage() function
+getImage();
